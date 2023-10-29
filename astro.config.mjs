b71c0cc250645bc4cac01830a9d0e3d984cb9349
Jonathan Bernardi <jon@jonbernardi.com>
2023-10-29 10:54:21 -0700

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig, squooshImageService } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-  ],
+  site: process.env.SITE || 'https://jonbernardi.com',
+  integrations: [tailwind(), sitemap()],
   image: {
-    service: squooshImageService(),
-  },
+    service: squooshImageService()
+  }
 });
